81c6d43ab047364357d648c755f5606842df9c04
dhb220 <dhb220@gmail.com>
2020-10-27 03:27:10 -0500

    docker update

--- nginx/init-letsencrypt.sh
+++ nginx/init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(docker.cmpartners.biz)
+domains=(198.58.106.125)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="dhb220@gmail.com" # Adding a valid address is strongly recommended
