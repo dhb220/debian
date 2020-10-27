c18af48c6ab1721f499efe472ea1b1ef8094dca0
dhb220 <dhb220@gmail.com>
2020-10-27 12:18:56 -0500

    DNS resolved

--- nginx/init-letsencrypt.sh
+++ nginx/init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(198.58.106.125)
+domains=(docker.cmpartners.biz)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="dhb220@gmail.com" # Adding a valid address is strongly recommended
